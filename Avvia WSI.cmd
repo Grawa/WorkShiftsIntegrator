3ca57ec4b9fc4950c1ac16a94db28d7cf278ea94
Grawa <014501450145Aa>
2019-10-27 13:20:26 +0100

    aggiunto avvia wsi batch

---
+++ Avvia WSI.cmd
@@ -0,0 +1,11 @@
+echo off
+cls
+
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+
+echo. Work shifts integrator - Logs:
+echo.
+
+py WSI.py
+
+pause>nul
--- WSI.py
+++ WSI.py
@@ -690,3 +690,5 @@ if __name__ == "__main__":
     window3 = UiEliminaVecchiDB()
     window.show()
     app.exec()
+
+
